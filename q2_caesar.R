cb0b75f880a7a18f5d5a29b5bce4ca2071c7a4a1
DeVcB13d <devcb@pg.cusat.ac.in>
2023-10-18 22:08:11 +0530

    added few R

---
+++ q2_caesar.R
@@ -0,0 +1,6 @@
+# Question 2: String Encryption
+
+# Write a program that reads a sentence from the user and encrypts it using a simple
+# Caesar cipher. The user can specify the shift value. Implement the encryption such that
+# only alphabetic characters are shifted, while maintaining their case.
+
