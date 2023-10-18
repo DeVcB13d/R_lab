10d0e1a6ae5dfea615523ead83f3cee0e952938d
DeVcB13d <devcb@pg.cusat.ac.in>
2023-10-19 00:02:59 +0530

    complete_cycle_1

--- q2_caesar.R
+++ q2_caesar.R
@@ -1,6 +1,45 @@
-# Question 2: String Encryption
+charAt <- function(sentence,index){
+    c <- substr(sentence,index,index)
+}
 
-# Write a program that reads a sentence from the user and encrypts it using a simple
-# Caesar cipher. The user can specify the shift value. Implement the encryption such that
-# only alphabetic characters are shifted, while maintaining their case.
+caeser_cipher <- function(sentence,shift){
+  for (i in 1:nchar(sentence)) {
+    if (tolower(charAt(sentence, i)) %in% letters) {
+      alphabet <- tolower(charAt(sentence, i))
+      position <- which(letters %in% alphabet) + shift
+      if (position > 26) {
+        position <- position - 26
+      }
+      if (i - 1 < 1) {
+        sentence <- paste0(
+          letters[position], substr(sentence, i + 1, nchar(sentence)
+          )
+        )
+      } else {
+        sentence <- paste0(
+          substr(sentence, 1, i - 1),
+          letters[position],
+          substr(sentence,
+          i + 1,
+          nchar(sentence)))
+      }
+    } else {
+      next
+    }
+  }
+  return(sentence)
+}
+cat("Enter your sentence : ")
+sentence <- readline()
 
+cat("Enter the shift : ")
+shift <- readline()
+
+shift <- as.numeric(shift)
+
+shifted_sentence <- caeser_cipher(sentence, shift)
+
+
+cat("Before Caeser Cipher Encryption : ", sentence, "\n")
+
+cat("After Caeser Cipher Encryption : ", shifted_sentence)
